9778bdbea3699dafdc0bcc02fc8af75eaa565e51
shanoukhavi <shanoukh5@gmail.com>
2024-07-04 00:16:15 +0530

    done changes

--- admin_panel.php
+++ admin_panel.php
@@ -1,117 +1,110 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel</title>
-    <link rel="stylesheet" href="admin_panel.css" />
+    <link rel="stylesheet" href="admin_panel.css">
     <style>
-      body {
-        margin: 0;
-        font-family: Arial, sans-serif;
-      }
+        body {
+            margin: 0;
+            font-family: Arial, sans-serif;
+            display: flex;
+        }
 
-      .sidebar {
-        height: 100%;
-        width: 200px;
-        position: fixed;
-        top: 0;
-        left: 0;
-        background-color: #f5f5f5;
-        padding: 20px;
-      }
+        .sidebar {
+            height: 100vh;
+            width: 200px;
+            position: fixed;
+            top: 0;
+            left: 0;
+            background-color: #333;
+            color: #fff;
+            padding: 20px;
+            box-shadow: 2px 0 5px rgba(0,0,0,0.1);
+        }
 
-      .sidebar h2 {
-        text-align: center;
-      }
+        .sidebar h2 {
+            text-align: center;
+            margin-bottom: 20px;
+            color: #ff9800;
+        }
 
-      .sidebar ul {
-        list-style-type: none;
-        padding: 0;
-      }
+        .sidebar ul {
+            list-style-type: none;
+            padding: 0;
+        }
 
-      .sidebar a {
-        display: block;
-        padding: 10px;
-        text-decoration: none;
-        color: #000;
-      }
+        .sidebar a {
+            display: block;
+            padding: 10px;
+            text-decoration: none;
+            color: #fff;
+            transition: background 0.3s;
+        }
 
-      .sidebar a:hover {
-        background-color: #e0e0e0;
-      }
+        .sidebar a:hover {
+            background-color: #575757;
+        }
 
-      .submenu ul {
-        display: none;
-      }
+        .submenu ul {
+            display: none;
+        }
 
-      .submenu:hover ul {
-        display: block;
-      }
+        .submenu:hover ul {
+            display: block;
+        }
 
-      .sidebar li ul {
-        padding-left: 20px;
-      }
+        .sidebar li ul {
+            padding-left: 20px;
+        }
+
+        .content {
+            margin-left: 220px;
+            padding: 20px;
+            width: calc(100% - 220px);
+        }
     </style>
-  </head>
-
-  <body>
+</head>
+<body>
     <div class="sidebar">
-      <h2>Menu</h2>
-      <ul>
-        <li class="submenu">
-          <a href="#">Students</a>
-          <ul>
-            <li>
-              <a href="http://localhost/phpglab/index3.php"
-                >Add and view Students</a
-              >
+        <h2>Menu</h2>
+        <ul>
+            <li class="submenu">
+                <a href="#">Students</a>
+                <ul>
+                    <li><a href="http://localhost/phpglab/index3.php">Add and View Students</a></li>
+                </ul>
             </li>
-          </ul>
-        </li>
-
-        <li class="submenu">
-          <a href="#">Staff</a>
-          <ul>
-            <li>
-              <a href="http://localhost/phpglab/index2.php"
-                >Add and view staffs</a
-              >
+            <li class="submenu">
+                <a href="#">Staff</a>
+                <ul>
+                    <li><a href="http://localhost/phpglab/index2.php">Add and View Staff</a></li>
+                </ul>
             </li>
-          </ul>
-        </li>
-
-        <li class="submenu">
-          <a href="#">Class Reports</a>
-          <ul>
-            <li>
-              <a href="http://localhost/phpglab/index4.php"
-                >Add and view reports</a
-              >
+            <li class="submenu">
+                <a href="#">Class Reports</a>
+                <ul>
+                    <li><a href="http://localhost/phpglab/index4.php">Add and View Reports</a></li>
+                </ul>
             </li>
-          </ul>
-        </li>
-      </ul>
+        </ul>
     </div>
 
     <div class="content">
-      <?php
-    require '_dbconnect.php';
-    echo "this";
-    
-    
-    $sql="SELECT *FROM `students`";
-    $result=mysqli_query($conn,$sql);
-    
-    $num=mysqli_num_rows($result);
-    
-    echo $num;
-    ?>
+        <?php
+        require '_dbconnect.php';
+        echo "this";
+        
+        $sql = "SELECT * FROM `students`";
+        $result = mysqli_query($conn, $sql);
+        
+        $num = mysqli_num_rows($result);
+        
+        echo $num;
+        ?>
+    </div>
 
-
-  </div>
-
-  
     <script src="adminscript.js"></script>
-  </body>
+</body>
 </html>
